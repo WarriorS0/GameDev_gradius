b7b72f00d63d66fb024d9f319c6b08034a1276a6
Camille Fourmaintraux <camille.fourmaintraux@etu.univ-grenoble-alpes.fr>
2026-06-20 20:52:28 +0200

    REFACTOR: update ISU.Coord to not extends ISU.Dimensions as it doesn't makes sense.

--- Moteur/src/game/graphics/MainPaint.java
+++ Moteur/src/game/graphics/MainPaint.java
@@ -1,6 +1,7 @@
 package game.graphics;
 
 import java.awt.Dimension;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import engine.controller.Controller;
@@ -10,6 +11,7 @@ import engine.entity.Entity;
 import engine.graphics.FollowerLabel;
 import engine.graphics.FpsManager;
 import engine.graphics.Hud;
+import engine.graphics.HudElement;
 import engine.graphics.Label;
 import engine.graphics.PixelCoordinate;
 import engine.graphics.View;
@@ -156,7 +158,9 @@ public class MainPaint implements Runnable {
 
 		Hud hud = new Hud();
 		// Exemple de label avec le fps
-		Label labelFPS = new Label(() -> "FPS " + fpsC.getFps(), new PixelCoordinate(10, 10), Colors.white, false);
+		Label labelDebug = new Label(() -> "'TAB' to toggle debug mode.", new PixelCoordinate(10, 10), Colors.white, false);
+		hud.add(labelDebug);
+		Label labelFPS = new Label(() -> "FPS " + fpsC.getFps(), new PixelCoordinate(10, 20), Colors.white, false);
 		hud.add(labelFPS);
 		FollowerLabel fbPacman = new FollowerLabel(() -> pacman.debugInfo(), Colors.white, pacman, 0, 10);
 		hud.add(fbPacman);
@@ -206,7 +210,11 @@ public class MainPaint implements Runnable {
 
 		KeyManager km = new KeyManager();
 		km.addDelegate(new Controller(pacmanStunt));
-		km.bind(VirtualKeyCodes.VK_TAB, () -> labelFPS.setVisibility(!labelFPS.isVisible()));
+		km.bind(VirtualKeyCodes.VK_TAB, () -> {
+			labelFPS.setVisibility(!labelFPS.isVisible());
+			fbPacman.setVisibility(!fbPacman.isVisible());
+			fbBlinky.setVisibility(!fbBlinky.isVisible());
+		});
 		// Appuyer sur tab fait appraître/disparaître le label de fps.
 
 		MouseManager mm = new MouseManager();
