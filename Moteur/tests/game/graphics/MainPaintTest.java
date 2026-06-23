a50bd1321a38f4b0f855b48908e8470d43e0c495
Antoni Maitre <antoni.maitre@etu.univ-grenoble-alpes.fr>
2026-06-23 13:30:25 +0200

    ?

--- Moteur/tests/game/graphics/MainPaintTest.java
+++ Moteur/tests/game/graphics/MainPaintTest.java
@@ -109,7 +109,7 @@ public class MainPaintTest implements Runnable {
 		// View
 		// =========================
 
-		View view = new View(null);
+		View view = new View();
 
 		view.add(new ShipAvatar(ship));
 		view.add(new CannonAvatar(topCannon));
