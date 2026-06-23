c979fcfd50504ea5e8b44712c255e01dac4078ed
WarriorS0 <achille.blanc@yahoo.com>
2026-06-23 17:55:15 +0200

    added ZOrder in Avatar, can be modified with setter

--- Moteur/src/engine/graphics/Avatar.java
+++ Moteur/src/engine/graphics/Avatar.java
@@ -4,15 +4,22 @@ import engine.entity.Entity;
 import oop.graphics.Graphics;
 
 public abstract class Avatar {
-
+	
+	final static int MAX_ZORDER = 9;
 	protected Entity entity;
+	private int z_order;
 
 	protected Avatar(Entity entity) {
 		this.entity = entity;
+		z_order =  0;
 	}
 
 	public Entity entity() {
 		return entity;
+	}
+	
+	public int z_order() {
+		return z_order;
 	}
 
 	public boolean dead() {
@@ -29,6 +36,12 @@ public abstract class Avatar {
 		if (entity != null) {
 			entity.revive();
 		}
+	}
+	
+	protected void set_z_order(int z_order) {
+		if(z_order > MAX_ZORDER)
+			throw new IllegalArgumentException("The maximum Z_order is "+MAX_ZORDER);
+		this.z_order = z_order;
 	}
 
 	public abstract void paint(Graphics g);
--- Moteur/src/engine/graphics/View.java
+++ Moteur/src/engine/graphics/View.java
@@ -176,6 +176,23 @@ public class View {
 	}
 
 	/**
+	 * @return an list of lists of Avatar objects, sorted by z_order
+	 */
+	
+	private List<List<Avatar>> getByZorder() {
+		List<List<Avatar>> res = new ArrayList<>();
+
+		for (int i = 0; i < Avatar.MAX_ZORDER; i++) {
+			res.add(new ArrayList<>());
+		}
+
+		for (Avatar avatar : avatars) {
+			res.get(avatar.z_order()).add(avatar);
+		}
+		return res;
+	}
+
+	/**
 	 * Projects a world coordinate (cm) to screen pixels, using the same extent as
 	 * the scene currently rendered (the real view port, or the whole map in debug
 	 * overview). On a toric axis the point is unfolded around the rendered origin
@@ -312,8 +329,11 @@ public class View {
 			background.paint(g);
 		}
 
-		for (Avatar avatar : avatars) {
-			avatar.paint(g);
+		List<List<Avatar>> avatarsByZorder = this.getByZorder();
+		for(int i=0; i<avatarsByZorder.size(); i++) {
+			for(Avatar avatar: avatarsByZorder.get(i)) {
+				avatar.paint(g);
+			}
 		}
 	}
 
