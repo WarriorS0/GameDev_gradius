57ed923406e1437e707e89d9bb801818e46eec7d
= <=>
2026-06-24 02:15:18 +0200

    added classes after merge

---
+++ Moteur/src/game/gradius/entity/DragonBody.java
@@ -0,0 +1,19 @@
+package game.gradius.entity;
+
+import engine.entity.Entity;
+
+class DragonBody extends Entity {
+
+	public DragonBody() {
+		super("dragon_body");
+		
+		setSize(isu.new Dimension(5,3));
+	}
+
+	@Override
+	protected void setBounding() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Moteur/src/game/gradius/entity/DragonHead.java
@@ -0,0 +1,17 @@
+package game.gradius.entity;
+
+import engine.entity.Entity;
+
+class DragonHead extends Entity {
+
+	public DragonHead() {
+		super("dragon_head");
+	}
+
+	@Override
+	protected void setBounding() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
